7e4c55fe9486c26fd5225384ae5146c7ac04b0c6
Rajawant Yadav <rajawantyadav436@gmail.com>
2026-08-08 18:42:33 +0530

    UPDATED NAV

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Nav() {
   return (
     <nav className="site-nav">
-      <a href="#booking" className="nav-mark">CONTACT US</a>
+      <a href="#booking" className="nav-mark">BOOK NOW</a>
       <Link to="/menu" className="nav-mark">MENU</Link>
     </nav>
   );
